692dcc0e53831634dc44bda833c1d2cd4563287e
Hyeong Cheol Kim <72899681+hyeongcheolkim@users.noreply.github.com>
2024-09-07 20:24:50 +0900

    Title: 색깔 트리, Time: 90ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240907/색깔 트리/color-tree.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+
+    private void solve(){
+        System.out.println("hi");
+    }
+
+    public static void main(String[] args) {
+        new Main().solve();
+    }
+}
