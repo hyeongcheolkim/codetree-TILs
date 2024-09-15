0e9e79d0b1f87fb320ac533a263be162fb29ee02
Hyeong Cheol Kim <72899681+hyeongcheolkim@users.noreply.github.com>
2024-09-15 20:08:07 +0900

    Title: 루돌프의 반란, Time: 290ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 240915/루돌프의 반란/rudolph-rebellion.java
+++ 240915/루돌프의 반란/rudolph-rebellion.java
@@ -224,10 +224,6 @@ public class Main {
             board[Rr][Rc] = -1;
 
             for(int i=1;i<=p;++i){
-                if(exitCount == p){
-                    System.out.println("exit!");
-                    break;
-                }
                 if(turn <= stun.get(i)){
                     // System.out.println("Santa stun" + String.valueOf(i));
                     continue;
@@ -250,6 +246,10 @@ public class Main {
                     board[nr][nc] = i;
                     santaLocations.put(i, new int[]{nr, nc});
                 }
+            }
+            if(exitCount == p){
+                System.out.println("exit!");
+                break;
             }
 
             for(int i=1;i<=p;++i){
